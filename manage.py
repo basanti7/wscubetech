9caaf3dcffd7d457866cb4821f1724cd989571b1
basanti7 <basantistore7@gmail.com>
2023-07-28 23:35:08 +0530

    initial commit

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """Run administrative tasks.."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wscubetech.settings')
     try:
         from django.core.management import execute_from_command_line
